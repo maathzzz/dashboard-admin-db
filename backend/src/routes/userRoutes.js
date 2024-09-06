0714e13c5171ec0387a4b5d83dd182687ec87d01
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-09-06 20:57:35 -0300

    feat: add docker compose

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,14 +3,14 @@ const userController = require("../controllers/userController");
 
 const userRoutes = Router();
 
-userRoutes.post("/register", async (req, res) => {
-    // await userController.createUser(req, res);
-    return res.json({ message: "Registrar usuário" });
-});
-
 userRoutes.get("/list", async (req, res) => {
     // await userController.getUsers(req, res);
     return res.json({ message: "Listar todos os usuários" });
+});
+
+userRoutes.post("/register", async (req, res) => {
+    // await userController.createUser(req, res);
+    return res.json({ message: "Registrar usuário" });
 });
 
 module.exports = userRoutes;
