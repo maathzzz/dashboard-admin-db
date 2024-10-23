9dd9ef241c3769c631e86e0cea03e20b0f3f8afb
Matheus A. Favero <matheusdeamorim20@gmail.com>
2024-10-23 19:58:54 -0300

    feat(admin): add routes & remaining methods

--- backend/src/controllers/AdminController.js
+++ backend/src/controllers/AdminController.js
@@ -2,6 +2,16 @@ const { PrismaClient } =  require('@prisma/client');
 const prisma =  new PrismaClient();
 
 class AdminController {
+    async getAdmins(req, res) {
+        try {
+            const admin = await prisma.admin.findMany();
+            return res.json(admin);
+        } catch (error) {
+            console.error("Erro ao listar administradores:", error);
+            return res.status(500).json({ error: "Erro ao listar administradores." });
+        }
+    }
+    
     async create(req, res) {
         const {
             name,
@@ -34,15 +44,35 @@ class AdminController {
         const admin = await prisma.admin.delete({
             where: { id: parseInt(id, 10) },
         });
-        return res.status(200).json({ message: "Usuário deletado com sucesso.", admin });
+        return res.status(200).json({ message: "Admin deletado com sucesso.", admin });
         } catch (error) {
-        console.error("Erro ao deletar usuário:", error);
+        console.error("Erro ao deletar Admin:", error);
         return res.status(500).json({ 
-            error: "Erro ao deletar usuário.",
+            error: "Erro ao deletar Admin.",
             message: "Error"
         });
         }
     }
+
+    async update(req, res) {
+        const { id } = req.params;
+        const {
+        name,
+        email,
+        password,
+        } = req.body;
+
+        try {
+            const admin = await prisma.admin.update({
+                where: { id: parseInt(id, 10) },
+                data: { name, email, password },
+            });
+            return res.status(200).json({ message: "Admin atualizado com sucesso.", admin });
+        } catch (error) {
+        console.error("Erro ao atualizar Admin:", error);
+        return res.status(500).json({ error: "Erro ao atualizar Admin.", message: "Error" });
+        }
+  }
 }
 
 module.exports = AdminController;
--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -4,12 +4,20 @@ const adminController = require("../controllers/AdminController");
 const adminRoutes = Router();
 const admin =  new adminController();
 
+adminRoutes.get("/list", async (req, res) => {
+  await admin.getAdmins(req, res);
+});
+
 adminRoutes.post("/register", async (req, res) => {
   await admin.create(req, res);
 });
 
 adminRoutes.delete("/delete/:id", async (req, res) => {
   await admin.delete(req, res);
+})
+
+adminRoutes.put("/update/:id", async (req, res) => {
+  await admin.update(req, res);
 })
 
 module.exports =  adminRoutes;
